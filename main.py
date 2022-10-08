7914da41610dd228a4cf8768204eed75ef6e00eb
Mark <86335247+Mokoth@users.noreply.github.com>
2022-10-08 02:52:19 +0000

    Error types and handling

--- main.py
+++ main.py
@@ -11,3 +11,22 @@ print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 # Using triple quote """ for big chunk of text with gaps/line breaks
 print("""Anything that  starts with quotes, and ends in three quotes can
 span many lines and even contain " symbols within it without freaking anything out!""")
+
+# Common Errors
+
+# NameError ~ get the name of a function wrong/misspell/get capitalization wrong
+# SyntaxError ~ have the order of symbols wrong/forget () or " "
+"""
+Print("What could go wrong?")
+print "Hello Error"
+print(Please work)
+"""
+# Fix my code!
+print("What could go wrong?")
+print ("Hello Error")
+print("Please work")
+
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
