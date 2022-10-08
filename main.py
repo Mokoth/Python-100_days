49518825b194780f1c77da383864a033cdad5fae
Mark <86335247+Mokoth@users.noreply.github.com>
2022-10-08 02:36:20 +0000

    Show comments and multiple line prints

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # First python code ~ day 1
 print("Hello, Mom")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+# Multiple Print Statements
+print("Well we")
+print("just use more lines")
+print("of code")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+# Using triple quote """ for big chunk of text with gaps/line breaks
+print("""Anything that  starts with quotes, and ends in three quotes can
+span many lines and even contain " symbols within it without freaking anything out!""")
