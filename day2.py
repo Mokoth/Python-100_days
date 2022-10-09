cc5d6db0c8bde64cc39caf3dc485f79f8235ed87
Mokoth <onyach.okoth1@outlook.com>
2022-10-09 09:25:45 +0300

    Input and variables

---
+++ day2.py
@@ -0,0 +1,14 @@
+# Day 2 of 100 - Input and Variables and day 2 challenge
+
+# input = ("What's your name?: ")
+
+# Assigning myName variable to input
+print("Getting to know you!")
+print()
+myName = input("What's your name?: ")
+
+favoriteFood = input("What's your favorite food?:")
+favoriteMusic = input("What's your favorite song?: ")
+location = input("Where do you live?: ")
+# Printing variables
+print(myName + " likes " + favoriteFood + ", and only enjoys " + favoriteMusic + " song." + "Currently residing in the bay areas of " + location)
