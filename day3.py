63cc0030ef319f99a69efcc0ea7fef3b6a9997b9
Mark <86335247+Mokoth@users.noreply.github.com>
2023-11-08 07:27:41 +0300

    Update day3.py

--- day3.py
+++ day3.py
@@ -2,15 +2,20 @@
 # Combine strings and variables => using comma/+
 
 print("=== Your Song Generator ===")
+
 print("""You'll be asked a bunch of questions
 then we'll make you up an amazing
 song, totally copyright free 😭""")
+
 print()
+
 person = input("Name a person famous for something good: ")
 thing = input("Name a thing they did: ")
 place = input("Name a place you like: ")
 rhyme = input("Give me a verb that rhymes with your person's name: ")
+
 print()
+
 print("There was a person called", person)
 print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
 
@@ -22,6 +27,7 @@ burntFood = input("How do you describe burnt food?")
 householdItem = input("Name something in your house: ")
 
 print()
+
 print("Tonight's dinner:")
 
 print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
