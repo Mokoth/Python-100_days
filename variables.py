72abe5c87b66a732ee922d6f97df7cbce8dd9ca7
Mokoth <onyach.okoth1@outlook.com>
2023-11-13 00:00:48 +0300

    update variables file

--- variables.py
+++ variables.py
@@ -1,14 +1,22 @@
-# Day 2 of 100 - Input and Variables and day 2 challenge
+# Variables
 
-# input = ("What's your name?: ")
+city = 'Kansas'
 
-# Assigning myName variable to input
+# Input -> using input
+
+
+# TODO Assigning myName variable to input
 print("Getting to know you!")
+
 print()
+
 myName = input("What's your name?: ")
 
 favoriteFood = input("What's your favorite food?:")
+
 favoriteMusic = input("What's your favorite song?: ")
+
 location = input("Where do you live?: ")
+
 # Printing variables
 print(myName + " likes " + favoriteFood + ", and only enjoys " + favoriteMusic + " song." + "Currently residing in the bay areas of " + location)
